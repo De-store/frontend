defe32ab3874a8547a9d9fcab21a147a71cfed17
suraj singla <suraj.singla@mckinleyrice.co>
2021-07-28 00:52:31 +0530

    Revert "remove ipfs"
    This reverts commit df8551e6a1310f4922a5cdaa0ef6141dab94ccea.

---
+++ client/src/services/Ipfs.ts
@@ -0,0 +1,23 @@
+
+import axios from 'axios';
+import { create } from 'ipfs-http-client';
+
+const ipfs = create({ host: 'ipfs.infura.io', port: 5001, protocol: 'https' });
+
+export const addToIPFS = async (data: any) => {
+    try {
+        const IPFS = await ipfs.add(data)
+        return IPFS
+    } catch (err) {
+        throw err
+    }
+}
+
+export const fetchFromIPFS = async (path: string) => {
+    try {
+        const response = await axios.get(`https://gateway.ipfs.io/ipfs/${path}`)
+        return response.data
+    } catch (err) {
+        throw err
+    }
+}

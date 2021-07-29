36f31ee7e74c6846d111cc22d0f02528c5f75837
suraj singla <suraj.singla@mckinleyrice.co>
2021-07-29 21:26:05 +0530

    update public IPFS gateway

--- client/src/utils/IpfsUrl.ts
+++ client/src/utils/IpfsUrl.ts
@@ -1,3 +1,4 @@
 export const createIpfsUrl = (hash: string): string => {
-    return `https://gateway.ipfs.io/ipfs/${hash}`
+    // return `https://gateway.ipfs.io/ipfs/${hash}`
+    return `https://ipfs.infura.io/ipfs/${hash}`
 }
